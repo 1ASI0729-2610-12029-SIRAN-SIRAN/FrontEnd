8bfd7e1540cce685de961cce184fd6b3b18b01fc
unknown <reppollio@gmail.com>
2026-05-03 18:07:41 -0500

    feat: added environments, db.json and configuration in routes.json

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  fakeDatabaseProviderApiBaseUrl: 'http://localhost:8080/api/v1',
+  fakeDatabaseProviderUserEndpointPath: '/users',
+  fakeDatabaseProviderSubscriptionPlanEndpointPath: '/subscriptionPlans',
+  fakeDatabaseProviderUserSubscriptionPlansEndpointPath: '/userSubscriptions',
+  fakeDatabaseProviderBabyEndpointPath: '/babies',
+  fakeDatabaseProviderDeviceEndpointPath: '/devices',
+  fakeDatabaseProviderHealthRecordEndpointPath: '/healthRecords',
+  fakeDatabaseFollowUpEndpointPath: '/followUps',
+  fakeDatabaseAlertRangeEndpointPath: '/alertRanges',
+  fakeDatabaseNotificationEndpointPath: '/notifications',
+  fakeDatabaseHealthReportEndpointPath: '/healthReports',
+};
